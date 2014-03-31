2183c2afa04ef9348950f078bd5dc92097bb972b
smoore8 <savionmoore8@gmail.com>
2014-03-31 10:09:42 -0400

    Last commit

---
+++ CheckZip.cs/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zips = new int[] { 10617, 12345, 43235, 10845, 13443, 12365, 64432, 12346, 12356, 76423 };
+            //Console.WriteLine("10617, 12345, 43235, 10845, 13443, 12365, 64432, 12346, 12356, 76423");
+            Console.Write("Enter your Zipcode: ");
+            String input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+
+            for (int i = 0; i < zips.Length; i++)
+            {
+                if (n == zips[i])
+                {
+                    Console.WriteLine("Yes we deliver to your Zip Code");
+                }
+                else if (n != zips[i])
+                {
+                    Console.WriteLine("We do not deliver to your Zip Code. Sorry.");
+                }
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ IntegerList.cs/IntegerList.cs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[] { 1, 5, 3, 2, 4 };
+
+
+            Array.Sort(num);
+            for (int i = 0; i < 5; i++)
+            {
+
+                //
+                Console.WriteLine(num[i]);
+
+
+            }
+
+            Array.Reverse(num);
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                //
+                Console.WriteLine(num[i]);
+
+
+            }
+
+        }
+    }
+}
